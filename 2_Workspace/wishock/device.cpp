31270cfe37372bf819f49c6c5fd5d7da05196915
HgN <hgnguyen37@gmail.com>
2017-09-12 18:08:37 +0700

    Remove 2 trunk folder. Add deviceStatus function.

--- 2_Workspace/wishock/device.cpp
+++ 2_Workspace/wishock/device.cpp
@@ -19,33 +19,53 @@ void deviceInit(void){
     pinMode(PIN_LED, OUTPUT);
     pinMode(PIN_DEVICE, OUTPUT);
 }
-
+/**
+ * Turn device ON
+ */
 void deviceOn(void){
     digitalWrite(PIN_DEVICE, LOW);
 }
-
+/**
+ * Turn device OFF
+ */
 void deviceOff(void){
     digitalWrite(PIN_DEVICE, HIGH);
 }
-
+/**
+ * Turn led ON
+ */
 void ledOn(void){
     digitalWrite(PIN_LED, LOW);
 }
-
+/**
+ * Turn led OFF
+ */
 void ledOff(void){
     digitalWrite(PIN_LED, HIGH);
 }
-
+/**
+ * Toggle device
+ */
 void deviceToggle(void){
     if(digitalRead(PIN_DEVICE) == HIGH)
         deviceOn();
     else
         deviceOff();
 }
-
+/**
+ * Toggle led
+ */
 void ledToggle(void){
     if(digitalRead(PIN_LED) == HIGH)
         deviceOn();
     else
         deviceOff();
+}
+/**
+ * @brief       Return device status
+ * @retval      HIGH
+ *              LOW
+ */
+uint8_t deviceStatus(void){
+    return digitalRead(PIN_DEVICE);
 }
