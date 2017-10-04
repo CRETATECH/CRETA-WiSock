1b484f85ceb515a0665842a19a4b6ee96abc4976
Nghia <ducnghia318@gmail.com>
2017-10-04 10:33:39 +0700

    finish function test (90%), need upgrade

--- 2_Workspace/test_case/function_test.py
+++ 2_Workspace/test_case/function_test.py
@@ -1,37 +1,54 @@
 import paho.mqtt.client as mqtt
 import json
-
+import sys
+import getopt
+import os
+import time
+import ast
+global send_num
 send_num    = 0
+global recv_num
 recv_num    = 0
+global timeout_num
 timeout_num = 0
+global done_num
 done_num    = 0
+global error_num
 error_num   = 0
-initialized = 0 # =1 if receive macID from topicTest
-sended      = 0
-timeout     = 0
+
 
 def funcTest(func):
+	global send_num
+	global timeout_num
 	mqttPublish(func, 1)
-        send_num = sended_num + 1
-	sended = 1
+        send_num = send_num + 1
+	sended   = 1
+	timeout  = 0
 	while sended == 1:
 		timeout = timeout + 1
-		if timeout > 500000:
+		if timeout > 100000000:
+			timeout_num = timeout_num + 1
 			timeout = 0
 			sended  = 0
+			print("ERROR: Msg timeout")
 			break
 	mqttPublish(func, 2)
-        send_num = sended_num + 1	
-	sended = 1
+        send_num = send_num + 1	
+	sended   = 1
 	while sended == 1:
 		timeout = timeout + 1
-		if timeout > 500000:
+		if timeout > 100000000:
+			timeout_num = timeout_num + 1
 			timeout = 0
 			sended  = 0
+			print("ERROR: Msg timeout")
 			break
 
 
 def mqttPublish(func, data):
+	# khai bao bien toan cuc
+	global ID
+	global topicOut
 	if func == 2:
 		func_str = 'Ctrl'
 	else:
@@ -47,19 +64,30 @@ def mqttPublish(func, data):
 
 #The callback for when receive CONNACK from server
 def on_connect(client, userdata, flags, rc):
-	print("Connected with result code" + str(rc))
+	print("SUCCESS: Connect to broker")
 	
 
 #the callback for when receive data from server
 def on_message(client, userdata, msg):
 	print("Message from: " + msg.topic + "\n")
 	if msg.topic == "topicTest":
-		print("--- macID: " + str(msg.payload) + "---")
-		client.subscribe(msg.topic + "/slave", qos = 0)
+		# khai bao bien toan cuc
+		global ID
+		global initialized
+		global topicOut
+		print("SUCCESS: Recv topic from topicTest")
+		print("-------- macID: " + str(msg.payload) + "------------")
+		print("Start to subscribe to topic: " + str(msg.payload) + "/slave")
+		client.unsubscribe("topicTest")
+		topicIn  = str(msg.payload) + "/slave"
+		client.subscribe(topicIn, qos = 0)
 		ID = str(msg.payload)
 		topicOut = str(msg.payload) + "master"
 		initialized = 1
 	else:
+		global recv_num
+		global error_num
+		global done_num
 		sended = 0
 		recv_num = recv_num + 1
 		parsed_json = json.loads(str(msg.payload))
@@ -69,15 +97,29 @@ def on_message(client, userdata, msg):
 		else:
 			done_num = done_num + 1
 		
-		
-		
+def on_publish(client, userdata, mid):
+	print("SUCCESS: Publish msg")
+
+def on_subscribe(client, userdata, mid, granted_qos):
+	print("SUCCESS: Subscribe topic")
+
+print("-------------------CRETA----------------------")
+print("-                                            -")
+print("-------------------WiSock---------------------")
+print("-                                            -")
+print("-----------------Function Test----------------\n")
+print("START")
+global initialized		
+initialized = 0		
 client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-print("Waiting to receive macID")
+client.on_connect   = on_connect
+client.on_message   = on_message
+client.on_publish   = on_publish
+client.on_subscribe = on_subscribe
 client.connect ("iot.eclipse.org", 1883, 60)
 client.subscribe("topicTest", qos = 0)
 client.loop()
+print("Waiting to receive macID")
 
 while 1:
 	if initialized == 1:
@@ -87,12 +129,14 @@ while 1:
 		funcTest(2)
 		client.loop()
 		break
+	client.loop()
+
 
 #write log
 print("---------------------------------------------------------")
 print("-------------------------RESULT--------------------------")
 print("| Msg Sent | Msg Recv | Msg Timeout | Msg Suc | Msg Err |") 
-print("|    " + str(send_num) + "     |    " + str(recv_num) + "     |    " + str(timeout_num) + "     |    " + str(done_num) + "     |    " + str(error_num) + "     |") 
+print("|    " + str(send_num) + "     |    " + str(recv_num) + "     |       " + str(timeout_num) + "     |    " + str(done_num) + "    |    " + str(error_num) + "    |") 
 print("--------------------------END----------------------------")
 
 
